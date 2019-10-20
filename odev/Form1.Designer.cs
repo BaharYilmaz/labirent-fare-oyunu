7244b797bc357d536150bc1d680307ef33187922
Bahar Yılmaz <baharr.yilmaz48@gmail.com>
2019-10-20 14:57:58 +0300

    first commit

--- odev/Form1.Designer.cs
+++ odev/Form1.Designer.cs
@@ -29,12 +29,76 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.dgv_labirent = new System.Windows.Forms.DataGridView();
+            this.btn_basla = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer_labirent = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_labirent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv_labirent
+            // 
+            this.dgv_labirent.AllowUserToAddRows = false;
+            this.dgv_labirent.AllowUserToDeleteRows = false;
+            this.dgv_labirent.AllowUserToResizeColumns = false;
+            this.dgv_labirent.AllowUserToResizeRows = false;
+            this.dgv_labirent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_labirent.ColumnHeadersVisible = false;
+            this.dgv_labirent.Location = new System.Drawing.Point(31, 32);
+            this.dgv_labirent.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgv_labirent.Name = "dgv_labirent";
+            this.dgv_labirent.ReadOnly = true;
+            this.dgv_labirent.RowHeadersVisible = false;
+            this.dgv_labirent.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dgv_labirent.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dgv_labirent.Size = new System.Drawing.Size(620, 573);
+            this.dgv_labirent.TabIndex = 1;
+            // 
+            // btn_basla
+            // 
+            this.btn_basla.Location = new System.Drawing.Point(861, 148);
+            this.btn_basla.Name = "btn_basla";
+            this.btn_basla.Size = new System.Drawing.Size(183, 69);
+            this.btn_basla.TabIndex = 2;
+            this.btn_basla.Text = "BAŞLA";
+            this.btn_basla.UseVisualStyleBackColor = true;
+            this.btn_basla.Click += new System.EventHandler(this.btn_basla_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(858, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // timer_labirent
+            // 
+            this.timer_labirent.Tick += new System.EventHandler(this.timer_labirent_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1216, 718);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_basla);
+            this.Controls.Add(this.dgv_labirent);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_labirent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.DataGridView dgv_labirent;
+        private System.Windows.Forms.Button btn_basla;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer_labirent;
     }
 }
 
--- odev/Form1.cs
+++ odev/Form1.cs
@@ -12,9 +12,233 @@ namespace odev
 {
     public partial class Form1 : Form
     {
+        Image imgYol = Image.FromFile(@"C: \Users\BHR\source\repos\odev\odev\resources\yol_.jpg");
+        Image imgLabirent = Image.FromFile(@"C:\Users\BHR\source\repos\odev\odev\resources\maze_.jpg");
+        Image imgFare1 = Image.FromFile(@"C:\Users\BHR\source\repos\odev\odev\resources\mice1.png");
+        Image imgFare2 = Image.FromFile(@"C:\Users\BHR\source\repos\odev\odev\resources\mice2.png");
+        Image imgPeynir = Image.FromFile(@"C:\Users\BHR\source\repos\odev\odev\resources\cheese.png");
+        
+        int fareX;
+        int fareY;
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgv_olustur();
+            timer_labirent.Interval = 200;
+
+        }
+
+        public void dgv_olustur()
+        {
+            dgv_labirent.Columns.Clear();
+            dgv_labirent.Rows.Clear();
+
+            for (int i = 0; i < 12; i++)
+            {
+                DataGridViewImageColumn resim = new DataGridViewImageColumn();
+                resim.ImageLayout = DataGridViewImageCellLayout.Stretch;
+                dgv_labirent.Columns.Add(resim);
+
+            }
+            dgv_labirent.Rows.Add(12);
+           
+            dgv_labirent.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            foreach (DataGridViewRow dgvRow in dgv_labirent.Rows)
+            {
+                dgvRow.Height = dgv_labirent.Columns[0].Width;
+            }
+
+           
+            labirentOlustur();
+
+        }
+        public void labirentOlustur()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = 0; j < 12; j++)
+                {
+                    if (dgv_labirent[i, j] != dgv_labirent[6, 7])
+                    {
+                        dgv_labirent[i, j].Value = imgYol; dgv_labirent[i, j].Tag = 1;// yol tag 1
+
+                    }
+                    else
+                    {
+                        dgv_labirent[i, j].Value = imgYol;
+                        dgv_labirent[i, j].Value = imgPeynir; dgv_labirent[i, j].Tag = 0;//peynir tag 0
+                    }
+
+                }
+
+            }
+
+            for (int i = 0; i < 12; i++)//üst-alt duvar
+            {
+                dgv_labirent[i, 0].Value = imgLabirent; dgv_labirent[i, 0].Tag = 100;
+                dgv_labirent[i, 11].Value = imgLabirent; dgv_labirent[i, 11].Tag = 100;
+            }
+            for (int i = 0; i < 12; i++)//yan duvarlar
+            {
+                dgv_labirent[0, i].Value = imgLabirent; dgv_labirent[0, i].Tag = 100;
+                dgv_labirent[11, i].Value = imgLabirent; dgv_labirent[11, i].Tag = 100;
+            }
+            for (int i = 2; i < 10; i++)
+            {
+                dgv_labirent[i, 2].Value = imgLabirent; dgv_labirent[i, 2].Tag = 100; //labirent duvar tag 100
+                dgv_labirent[i, 8].Value = imgLabirent; dgv_labirent[i, 8].Tag = 100;
+            }
+            for (int i = 3; i < 10; i++)
+            {
+                if (dgv_labirent[2, i] != dgv_labirent[2, 6])
+                {
+                    dgv_labirent[2, i].Value = imgLabirent; dgv_labirent[2, i].Tag = 100;
+                }
+            }
+            for (int i = 3; i < 6; i++)
+            {
+                dgv_labirent[i, 5].Value = imgLabirent; dgv_labirent[i, 5].Tag = 100;
+            }
+            for (int i = 4; i < 8; i++)
+            {
+                dgv_labirent[7, i].Value = imgLabirent; dgv_labirent[7, i].Tag = 100;
+            }
+            dgv_labirent[5, 4].Value = imgLabirent; dgv_labirent[5, 4].Tag = 100;
+            dgv_labirent[8, 4].Value = imgLabirent; dgv_labirent[8, 4].Tag = 100;
+            dgv_labirent[9, 4].Value = imgLabirent; dgv_labirent[9, 4].Tag = 100;
+            dgv_labirent[4, 7].Value = imgLabirent; dgv_labirent[4, 7].Tag = 100;
+            dgv_labirent[5, 9].Value = imgLabirent; dgv_labirent[5, 9].Tag = 100;
+            dgv_labirent[8, 9].Value = imgLabirent; dgv_labirent[8, 9].Tag = 100;
+            dgv_labirent[9, 6].Value = imgLabirent; dgv_labirent[9, 6].Tag = 100;
+            dgv_labirent[10, 6].Value = imgLabirent; dgv_labirent[10, 6].Tag = 100;
+
+            dgv_labirent[10, 10].Value = imgFare1; dgv_labirent[10, 10].Tag = 10;//fare tag 10
+            fareX = 10;
+            fareY = 10;
+
+
+        
+    }
+
+        private void btn_basla_Click(object sender, EventArgs e)
+        {
+            btn_basla.Enabled = false;
+            timer_labirent.Enabled = true;
+            timer_labirent.Start();
+        }
+
+        public int fareBul()
+        {
+            int kuzey = 0;
+            int guney = 0;
+            int bati = 0;
+            int dogu = 0;
+            int hucredegeri = 0;
+            DataGridViewImageCell yeniFareKonum = new DataGridViewImageCell();
+            yeniFareKonum.ImageLayout = DataGridViewImageCellLayout.Stretch;
+            yeniFareKonum.Value = imgFare1;
+            
+            kuzey = Convert.ToInt32(dgv_labirent.Rows[fareY - 1].Cells[fareX].Tag);
+            guney = Convert.ToInt32(dgv_labirent.Rows[fareY + 1].Cells[fareX].Tag);
+            bati = Convert.ToInt32(dgv_labirent.Rows[fareY].Cells[fareX - 1].Tag);
+            dogu = Convert.ToInt32(dgv_labirent.Rows[fareY].Cells[fareX + 1].Tag);
+
+            if(kuzey<=bati && kuzey<=dogu &&kuzey<=guney )
+            {
+                if(kuzey!=100)//duvar
+                {
+                    hucredegeri = kuzey;
+                    if(kuzey==0)//peynir
+                    {
+                        timer_labirent.Stop();
+                        timer_labirent.Enabled = false;
+                        return 1;
+                    }
+                    hucredegeri++;
+                    yeniFareKonum.Tag = hucredegeri;
+                    dgv_labirent.Rows[fareY - 1].Cells[fareX] = yeniFareKonum;
+                    dgv_labirent.Rows[fareY].Cells[fareX].Value = imgYol; //eski fare konumuna yok eklenir
+                    fareY--;
+                }
+               
+            }
+            else if (bati <= kuzey && bati <= dogu & bati <= guney)
+            {
+                if (bati != 100)
+                {
+                    hucredegeri = bati;
+                    if (bati == 0)
+                    {
+                        timer_labirent.Stop();
+                        timer_labirent.Enabled = false;
+                        return 1;
+                    }
+                    hucredegeri++;
+                    yeniFareKonum.Tag = hucredegeri;
+                    dgv_labirent.Rows[fareY].Cells[fareX - 1] = yeniFareKonum;
+                    dgv_labirent.Rows[fareY].Cells[fareX].Value = imgYol;
+                    fareX--;
+                }
+            }
+            else if (guney <= kuzey && guney <= bati && guney <= dogu)
+            {
+                if (guney != 100)
+                {
+                    hucredegeri = guney;
+                    if (guney == 0)
+                    {
+                        timer_labirent.Stop();
+                        timer_labirent.Enabled = false;
+                        return 1;
+                    }
+                    hucredegeri++;
+                    yeniFareKonum.Tag = hucredegeri;
+                    dgv_labirent.Rows[fareY + 1].Cells[fareX] = yeniFareKonum;
+                    dgv_labirent.Rows[fareY].Cells[fareX].Value = imgYol;
+                    fareY++;
+                }
+            }
+            else if (dogu <= kuzey && dogu <= bati && dogu <= guney)
+            {
+                if (dogu != 100 )
+                {
+                    hucredegeri = dogu;
+                    if (dogu == 0)
+                    {
+                        timer_labirent.Stop();
+                        timer_labirent.Enabled = false;
+                        return 1;
+                    }
+                    hucredegeri++;
+                    yeniFareKonum.Tag = hucredegeri;
+                    dgv_labirent.Rows[fareY].Cells[fareX + 1] = yeniFareKonum;
+                    dgv_labirent.Rows[fareY].Cells[fareX].Value = imgYol;
+                    fareX++;
+                }
+            }
+            return 0;
+
+        }
+
+        public void fareBulManuel()
+        { }
+
+        public void fareBulCiftKisi()
+        { }
+
+        private void timer_labirent_Tick(object sender, EventArgs e)
+        {
+            int returnValue = fareBul();
+            if(returnValue==1)
+            {
+                MessageBox.Show("PEYNİR BULUNDU!");
+            }
+
         }
     }
 }
